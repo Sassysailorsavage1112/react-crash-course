80720581fd2f49dde01f6967a7b3738f45530d01
Your Name <local@localhost>
2026-09-05 05:10:07 -0700

    updated CSS

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,8 @@
+import './Todo.css'
+
 function Todo() {
     return(
-       <div>
+       <div className="todo">
   <h2>Finish Frontend Simplified</h2>
   <span>Description</span>
   <button>Delete</button>
