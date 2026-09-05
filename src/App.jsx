adeb95eca528d50cc5cc3d5c407bb7056acff160
Your Name <local@localhost>
2026-09-05 04:18:44 -0700

    added reusable code

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
  <h1>My To Do List:</h1>
  <div>
   <h2>Finish Frontend Simplified</h2>
-  <button>delete</button>
+  <button>Delete</button>
+ </div>
+ <div>
+  <h2>Finish the Interview Section</h2>
+  <button>Delete</button>
+ </div><div>
+  <h2>Land a Job that pays more than $150,000</h2>
+  <button>Delete</button>
  </div>
     </div>
   );
