fd5bd806c81b125e39012f9c3cec7f660135a523
Your Name <local@localhost>
2026-09-08 23:29:42 -0700

    updated titles

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
     <>
  <Title />
  <div className="todo__wrapper">
- <Todo></Todo>
- <Todo></Todo>
- <Todo></Todo>
+ <Todo title="Finish Frontend Simplified" </Todo>
+ <Todo title= "Finish the Interview Section on Frontend Simplified"></Todo>
+ <Todo title= "Land a > $150,000k job that I love"></Todo>
 </div>
 {/* <Modal /> */}
     </>
