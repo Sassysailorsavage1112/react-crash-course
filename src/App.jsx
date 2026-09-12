909d7decc86c72db0c1a9ee3effd19d41979b38c
Your Name <local@localhost>
2026-09-12 04:36:19 -0700

    updated button

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
             console.log(event.target.value);
           }}
         />
-        <button>Add todo</button>
+        <button onClick={toggleModal}>Add todo</button>
       </div>
 
       <div className="todo__wrapper">
