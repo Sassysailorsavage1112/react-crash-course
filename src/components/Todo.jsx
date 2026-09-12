be57e14a2035e7fdaa2225a675b3396092a3bf2a
Your Name <local@localhost>
2026-09-11 20:53:57 -0700

    updated title

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,15 @@
 import './Todo.css'
 
 function Todo({title, paragraph}) {
-    function deleteTodo() {
-   console.log('deletTodo()')
+    function deleteTodo(id) {
+   console.log('deletTodo()', title
     }
 
     return(
        <div className="todo">
   <h2>{ title }</h2>
   <p>{paragraph}</p>
-  <button onClick={deleteTodo()}>Delete</button>
+  <button onClick={ () => deleteTodo(1)}>Delete</button>
  </div>
     ); 
 }
