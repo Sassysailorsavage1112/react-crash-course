771cc543edc8b1f4bb46a8239974be5e439544e2
Your Name <local@localhost>
2026-09-11 20:38:07 -0700

    updated Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,19 @@
-import './Todo.css'
+bimport './Todo.css'
 
 function Todo({title, paragraph}) {
-    console.log(title)
+    function deletTodo() {
+    console.log('deleteTodo'()')
+    }
+
     return(
        <div className="todo">
   <h2>{ title }</h2>
-  <p>{paragraph} </p>
-  <span>Description</span>
-  <button onClick={() =>  console.log('clicked')}>Delete</button>
+  <p>{paragraph}</p>
+  <button onClick={() => {
+  console.log("clicked")
+  }}>Delete</button>
  </div>
-    ) 
+    ); 
 }
 
 export default Todo
