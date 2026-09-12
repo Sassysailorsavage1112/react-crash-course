a5a24588c450b6f6718be94558d16e766c1df02b
Your Name <local@localhost>
2026-09-11 20:22:34 -0700

    console.log

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,7 +7,9 @@ function Todo({title, paragraph}) {
   <h2>{ title }</h2>
   <p>{paragraph} </p>
   <span>Description</span>
-  <button>Delete</button>
+  <button onClick={() => {
+    console.log('clicked')
+  }}>Delete</button>
  </div>
     ) 
 }
