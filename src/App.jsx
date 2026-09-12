24a6f19bd3f695c273c5895687ad1cddfd27be60
Your Name <local@localhost>
2026-09-11 19:57:35 -0700

    are you sure you want to add

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
   paragraph= "Apply to 100 jobs." />
 </div>
 {/* <Modal title="Are you sure you want to delete?" /> */}
+<Modal title="Are you sure you want to add?" />
     </>
   );
 }
