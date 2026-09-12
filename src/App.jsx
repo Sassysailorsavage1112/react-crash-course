7736d80dd6467c62344d3aec3ec1d73bcf8f0b2f
Your Name <local@localhost>
2026-09-11 22:13:22 -0700

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import logo from './logo.svg';
-import Todo from './components/Todo.jsx'
-import Title from './components/Title.jsx'
-import "./App.css";
-import Modal from './components/Modal.jsx'
+import Todo from './components/Todo.jsx';
+import Title from './components/Title.jsx';
+import './App.css';
+import Modal from './components/Modal.jsx';
 import React, { useState } from 'react';
+
 function App() {
   return (
     <div>
- <Title />
- <div>
-  <input type="text" onChange={(event) => {
-console.log(event.target.value)
-  }} />
-  <button>Add todo</button>
-  </div>
- <div className="todo__wrapper">
- <Todo title= "Finish Frontend Simplified" />
- <Todo title= "Finish the Interview Section on Frontend Simplified"/>
- <Todo title= "Land a > $150,000k job that I love"/>
-</div>
-{true && <Modal title="Are you sure you want to delete?" />}
+      <Title />
+
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button>Add todo</button>
+      </div>
+
+      <div className="todo__wrapper">
+        <Todo title="Finish Frontend Simplified" />
+        <Todo title="Finish the Interview Section on Frontend Simplified" />
+        <Todo title="Land a > $150,000k job that I love" />
+      </div>
+
+      {true && <Modal title="Are you sure you want to delete?" />}
+      {true && <Modal title="Confirm Delete" />}
     </div>
-    {true && <Modal title="Confirm Delete"}
-  </div>
   );
 }
 
