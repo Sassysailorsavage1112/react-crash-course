a64176a25998787d12da827cbe710b7fd4bad216
Your Name <local@localhost>
2026-09-16 22:59:27 -0700

    updated functions

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,19 @@
 function Counter() {
+   function increment() { 
+    console.log('incrementCounter()')
+
+   }
+
+    function decrement() {
+    console.log('decrementCounter()')
+    }
     return (
         <div>
-<button></button>
+<button onClick={decrement}>-</button>
 {0}
-<button></button>
+<button onClick={increment}>+</button>
 </div>
     )
 }
+
+export default Counter
