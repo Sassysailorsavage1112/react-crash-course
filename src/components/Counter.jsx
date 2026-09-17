9d91d1d408570e752b190b8272e58db3801b2e53
Your Name <local@localhost>
2026-09-16 23:31:46 -0700

    counter/count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,19 +3,18 @@ import React, { useState } from 'react';
 
 function Counter() {
     const [count, setCounter] = useState(0);
-
     function increment() {
-        setCount(count + 1);
+        setCounter(count + 1);
         console.log('incrementCounter()');
     }
 
-    function decrement() {
-        setCount(count - 1);
+    function decrementCounter() {
+        setCounter(count - 1);
         console.log('decrementCounter()');
     }
     return (
         <div>
-<button onClick={decrement}>-</button>
+<button onClick={decrementCounter}>-</button>
 {count}
 <button onClick={increment}>+</button>
 </div>
