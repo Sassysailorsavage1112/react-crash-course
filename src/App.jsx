57ab7bf1ee1743155ba833f13ffc70628c24d308
Your Name <local@localhost>
2026-09-16 20:53:44 -0700

    setshowmodal

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Modal from './components/Modal.jsx';
 import React, { useState } from 'react';
 
 function App() {
- const [showModal] = useState(true)
+ const [showModal, setShowModal] = useState(true)
 
   return (
     <div>
