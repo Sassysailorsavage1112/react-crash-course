97e25ca718038e6814d8b417bc3aa5dfb4ad4302
Your Name <local@localhost>
2026-09-17 23:47:34 -0700

    updated code

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  const [cart, setCart] = useState({ 
+    item: "apple",
+     quantity: 10, 
+    });
 
-  function incrementCounter() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
-
-  function decrementCounter() {
-    setCounter((prevCounter) => prevCounter - 1);
-  }
-
+    function addApple() {
+setCart(prevCart => ({
+  ...prevCart,
+  quantity: 1,
+    }))}
   return (
     <div>
-      <button onClick={decrementCounter}>-</button>
-      {counter}
-      <button onClick={incrementCounter}>+</button>
+      <button onClick={() => null}>-</button>
+      {cart.quantity}
+      {cart.item}
+      <button onClick={addApple}>+</button>
     </div>
   );
 }
